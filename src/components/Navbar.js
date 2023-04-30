4395db8b084dccd2f4f52c4c2ab8f01699dac4a4
Aboagye=Heming <hemingkwabena@gmail.com>
2023-04-30 19:01:04 +0000

    made my navbar responsive and added some functionalities to the mobile menu dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BsFacebook,
   BsInstagram,
@@ -8,11 +8,21 @@ import {
   BsTwitter,
   BsYoutube,
 } from "react-icons/bs";
+import { AiOutlineClose, AiOutlineBars } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const [logo, setLogo] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+    setLogo(!logo);
+  };
+
   return (
     <div className="flex justify-between items-center h-20 px-4">
-      <h1>HEMNEGY.</h1>
+      <div >
+        <h1 className={logo ? 'hidden' : 'block'}>HEMNEGY.</h1>
+      </div>
       <ul className="md:flex hidden">
         <li>Home</li>
         <li>Destination</li>
@@ -20,7 +30,7 @@ const Navbar = () => {
         <li>View</li>
         <li>Book</li>
       </ul>
-      <div>
+      <div> 
         <ul className="md:flex hidden">
           <li>
             <BsPerson size={20} />
@@ -33,22 +43,33 @@ const Navbar = () => {
       </div>
 
       {/* Humburger */}
-      <div className="md:flex hidden">
-        <img src="./src/assets/images/icon-cart.svg" alt="" size={20} />
+      <div onClick={handleNav} className="md:hidden z-10">
+        {nav ? (
+          <AiOutlineClose className="text-black" size={20} />
+        ) : (
+          <AiOutlineBars size={20} />
+        )}
       </div>
 
       {/* Mobile menu dropdown */}
-      <div className="absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col">
+      <div
+        onClick={handleNav}
+        className={
+          nav
+            ? "absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col"
+            : "absolute left-[-100%] "
+        }
+      >
         <ul>
           <h1>HEMNEGY.</h1>
-          <li>Home</li>
-          <li>Destination</li>
-          <li>Travel</li>
-          <li>View</li>
-          <li>Book</li>
+          <li className="border-b">Home</li>
+          <li className="border-b">Destination</li>
+          <li className="border-b">Travel</li>
+          <li className="border-b">View</li>
+          <li className="border-b">Book</li>
           <div className="flex flex-col">
-            <button className="my-6" >Search</button>
-            <button >Account</button>
+            <button className="my-6">Search</button>
+            <button>Account</button>
           </div>
           <div className="flex justify-between my-6">
             <BsFacebook className="icon" />
