db2d589159a6a6765cdf07850f1156d73f1eff25
Aboagye=Heming <hemingkwabena@gmail.com>
2023-05-02 13:34:58 +0000

    added an image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar";
+import jay from './assets/tobias-nii-kwatei-quartey-rpcCF2Asrx8-unsplash.jpg'
 
 
 function App() {
   return (
     <div>
      <Navbar/>
-    
+    <img src={jay} alt=""/>
     </div>
   );
 }
